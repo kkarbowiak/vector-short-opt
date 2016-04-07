8754e8944424cf09cb8147cb113da61e741b40f6
kkarbowiak <kkarbowiak@localhost>
2016-04-07 23:35:15 +0200

    Minor test case refactoring

--- test/unittest/source/test_vector_short_opt.cpp
+++ test/unittest/source/test_vector_short_opt.cpp
@@ -839,13 +839,15 @@ TEST_CASE("Insert raange", "[opt][insert][range]")
 {
     SECTION("Empty")
     {
+        int const v1 = 7;
+        int const v2 = 17;
+
+        vec4<int>::type::size_type n = 3;
+
+        vec4<int>::type v4;
+
         SECTION("To begin")
         {
-            int const v1 = 7;
-            vec4<int>::type::size_type n = 3;
-
-            vec4<int>::type v4;
-
             v4.insert(v4.begin(), n, v1);
 
             REQUIRE(v4.capacity() >= v4.size());
@@ -853,8 +855,6 @@ TEST_CASE("Insert raange", "[opt][insert][range]")
             REQUIRE(v4[0] == v1);
             REQUIRE(v4[1] == v1);
             REQUIRE(v4[2] == v1);
-
-            int const v2 = 17;
 
             v4.insert(v4.begin(), n, v2);
 
@@ -867,11 +867,6 @@ TEST_CASE("Insert raange", "[opt][insert][range]")
 
         SECTION("To end")
         {
-            int const v1 = 7;
-            vec4<int>::type::size_type n = 3;
-
-            vec4<int>::type v4;
-
             v4.insert(v4.end(), n, v1);
 
             REQUIRE(v4.capacity() >= v4.size());
@@ -879,8 +874,6 @@ TEST_CASE("Insert raange", "[opt][insert][range]")
             REQUIRE(v4[0] == v1);
             REQUIRE(v4[1] == v1);
             REQUIRE(v4[2] == v1);
-
-            int const v2 = 17;
 
             v4.insert(v4.end(), n, v2);
 
